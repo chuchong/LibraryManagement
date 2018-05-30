165d7e66bccaf7b553c2cd9bfb32303f8bbef4c9
chuchong <lisiyu201695@gmail.com>
2018-05-31 00:31:09 +0800

    正确安排了debug文件位置
    debug文件位置会影响能否连接上数据库

--- qt try/build-ui_outline-Desktop_Qt_5_6_3_MinGW_32bit-Debug/debug/moc_logindialog.cpp
+++ qt try/build-ui_outline-Desktop_Qt_5_6_3_MinGW_32bit-Debug/debug/moc_logindialog.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_LoginDialog_t {
-    QByteArrayData data[10];
-    char stringdata0[140];
+    QByteArrayData data[9];
+    char stringdata0[134];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -37,14 +37,13 @@ QT_MOC_LITERAL(4, 35, 8), // "password"
 QT_MOC_LITERAL(5, 44, 16), // "LoginDestruction"
 QT_MOC_LITERAL(6, 61, 22), // "on_LoginButton_clicked"
 QT_MOC_LITERAL(7, 84, 25), // "on_RegisterButton_clicked"
-QT_MOC_LITERAL(8, 110, 23), // "on_CancelButton_clicked"
-QT_MOC_LITERAL(9, 134, 5) // "debug"
+QT_MOC_LITERAL(8, 110, 23) // "on_CancelButton_clicked"
 
     },
     "LoginDialog\0LoginSignals\0\0accnount\0"
     "password\0LoginDestruction\0"
     "on_LoginButton_clicked\0on_RegisterButton_clicked\0"
-    "on_CancelButton_clicked\0debug"
+    "on_CancelButton_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -54,7 +53,7 @@ static const uint qt_meta_data_LoginDialog[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -62,14 +61,13 @@ static const uint qt_meta_data_LoginDialog[] = {
        2,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    2,   44,    2, 0x06 /* Public */,
-       5,    0,   49,    2, 0x06 /* Public */,
+       1,    2,   39,    2, 0x06 /* Public */,
+       5,    0,   44,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       6,    0,   50,    2, 0x08 /* Private */,
-       7,    0,   51,    2, 0x08 /* Private */,
-       8,    0,   52,    2, 0x08 /* Private */,
-       9,    2,   53,    2, 0x08 /* Private */,
+       6,    0,   45,    2, 0x08 /* Private */,
+       7,    0,   46,    2, 0x08 /* Private */,
+       8,    0,   47,    2, 0x08 /* Private */,
 
  // signals: parameters
     QMetaType::Void, QMetaType::QString, QMetaType::QString,    3,    4,
@@ -79,7 +77,6 @@ static const uint qt_meta_data_LoginDialog[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::QString, QMetaType::QString,    3,    4,
 
        0        // eod
 };
@@ -95,7 +92,6 @@ void LoginDialog::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id,
         case 2: _t->on_LoginButton_clicked(); break;
         case 3: _t->on_RegisterButton_clicked(); break;
         case 4: _t->on_CancelButton_clicked(); break;
-        case 5: _t->debug((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -143,13 +139,13 @@ int LoginDialog::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 6)
+        if (_id < 5)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
+        _id -= 5;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
+        if (_id < 5)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 6;
+        _id -= 5;
     }
     return _id;
 }
